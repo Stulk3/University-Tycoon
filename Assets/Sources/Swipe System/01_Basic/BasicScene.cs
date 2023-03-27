77b1a495ff683e0b81d75384e7f36657ab63fcd2
Big Boss <stulk7746@gmail.com>
2023-03-27 14:44:49 +0300

    ADD: Swipe System
    + CHANGE: Platform for Android

---
+++ Assets/Sources/Swipe System/01_Basic/BasicScene.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace SwipeableView
+{
+    public class BasicScene : MonoBehaviour
+    {
+        [SerializeField]
+        private UISwipeableViewBasic swipeableView = default;
+
+        void Start()
+        {
+            var data = Enumerable.Range(0, 20)
+                .Select(i => new BasicCardData
+                {
+                    color = new Color(Random.value, Random.value, Random.value, 1.0f)
+                })
+                .ToList();
+
+            swipeableView.UpdateData(data);
+        }
+
+        public void OnClickLike()
+        {
+            if (swipeableView.IsAutoSwiping) return;
+            swipeableView.AutoSwipe(SwipeDirection.Right);
+        }
+
+        public void OnClickNope()
+        {
+            if (swipeableView.IsAutoSwiping) return;
+            swipeableView.AutoSwipe(SwipeDirection.Left);
+        }
+    }
+}
